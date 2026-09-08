97f1bb3ab76d434935edaf703816ee41393f9438
Arsen Muk <arsenmuk@clickhouse.com>
2026-09-08 14:26:24 +0200

    Polish iceberg perf suite

--- tests/performance/write_small_commits.sql
+++ tests/performance/write_small_commits.sql
@@ -1,10 +1,5 @@
-INSERT INTO {database}.lineitem SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
-INSERT INTO {database}.lineitem SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
-INSERT INTO {database}.lineitem SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
-INSERT INTO {database}.lineitem SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
-INSERT INTO {database}.lineitem SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
-INSERT INTO {database}.lineitem SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
-INSERT INTO {database}.lineitem SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
-INSERT INTO {database}.lineitem SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
-INSERT INTO {database}.lineitem SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
-INSERT INTO {database}.lineitem SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
+INSERT INTO {database}.wr_small SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
+INSERT INTO {database}.wr_small SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
+INSERT INTO {database}.wr_small SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
+INSERT INTO {database}.wr_small SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
+INSERT INTO {database}.wr_small SELECT * FROM tpch10.lineitem LIMIT 100 SETTINGS max_threads = 1, max_insert_threads = 1;
